ead62cd997d94b6b3d0754a7d308797daf9c3e27
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-06-06 23:02:38 -0400

    add module scope-restricted function

--- src/vectors.rs
+++ src/vectors.rs
@@ -1,3 +1,12 @@
+
+// pub can be accessed externally after importing the mod
 pub fn print_something() {
     println!("something");
+    cant_find_me();
+}
+
+// without pub, this function is hidden, even after importing this module
+// but it can be used internally, as above
+fn cant_find_me() {
+    println!("I'm hidden to outside modules")
 }
