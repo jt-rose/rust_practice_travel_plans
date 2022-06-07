99d80dd2d0906d0f382835c7c24345d1b5a2e419
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-06-06 23:32:09 -0400

    clean up imports

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 mod vectors;
 
+use vectors::*;
+
 fn main() {
     println!("Hello, world!");
-    vectors::print_something();
-    let mut cities = vectors::get_cities();
+    print_something();
+    let mut cities = get_cities();
     println!("First city is {}", cities[0]);
 
-    for city in vectors::filter_visited_cities(&vectors::get_cities()).iter() {
+    for city in filter_visited_cities(&get_cities()).iter() {
         println!("{} has not been visited", city);
     }
 }
